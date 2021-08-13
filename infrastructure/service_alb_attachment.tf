5029c3eb7e4107fe0995c7868ae0e263fb8b2662
Ravi Khakhkhar <rkhakhkhar@equalexperts.com>
2021-08-14 01:48:42 +0530

    fix green alb_lb_target_group

--- infrastructure/service_alb_attachment.tf
+++ infrastructure/service_alb_attachment.tf
@@ -37,7 +37,7 @@ resource "aws_lb_target_group" "service_tg" {
 }
 
 resource "aws_lb_target_group" "service_green_tg" {
-  name     = "${local.name_prefix}-${local.service_name}-green-tg"
+  name     = "${local.name_prefix}-${local.service_name}-g-tg"
   port     = "8080"
   protocol = "HTTP"
   vpc_id   = local.vpc_id
